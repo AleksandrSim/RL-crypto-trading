96d61c6d8e39b5bea14ef529968682a5f03d912b
AleksandrSim <46563315+AleksandrSim@users.noreply.github.com>
2022-05-27 15:15:09 -0500

    dataset

--- simple_approach.py
+++ simple_approach.py
@@ -2,18 +2,16 @@ import pandas as pd
 import numpy as np
 import random
 from collections import deque
-from utils import TradingGraph, Write_to_file
 
 
 class CustomEnv:
     # A custom Bitcoin trading environment
-    def __init__(self, df, initial_balance=1000, lookback_window_size=50, Render_range=100):
+    def __init__(self, df, initial_balance=1000, lookback_window_size=50):
         # Define action space and state size and other custom parameters
         self.df = df.dropna().reset_index()
         self.df_total_steps = len(self.df) - 1
         self.initial_balance = initial_balance
         self.lookback_window_size = lookback_window_size
-        self.Render_range = Render_range  # render range in visualization
 
         # Action space from 0 to 3, 0 is hold, 1 is buy, 2 is sell
         self.action_space = np.array([0, 1, 2])
@@ -29,9 +27,6 @@ class CustomEnv:
 
     # Reset the state of the environment to an initial state
     def reset(self, env_steps_size=0):
-        self.visualization = TradingGraph(Render_range=self.Render_range)  # init visualization
-        self.trades = deque(maxlen=self.Render_range)  # limited orders memory for visualization
-
         self.balance = self.initial_balance
         self.net_worth = self.initial_balance
         self.prev_net_worth = self.initial_balance
@@ -82,33 +77,27 @@ class CustomEnv:
         current_price = random.uniform(
             self.df.loc[self.current_step, 'Open'],
             self.df.loc[self.current_step, 'Close'])
-        Date = self.df.loc[self.current_step, 'Date']  # for visualization
-        High = self.df.loc[self.current_step, 'High']  # for visualization
-        Low = self.df.loc[self.current_step, 'Low']  # for visualization
 
         if action == 0:  # Hold
             pass
 
-        elif action == 1 and self.balance > self.initial_balance / 100:
+        elif action == 1 and self.balance > 0:
             # Buy with 100% of current balance
             self.crypto_bought = self.balance / current_price
             self.balance -= self.crypto_bought * current_price
             self.crypto_held += self.crypto_bought
-            self.trades.append({'Date': Date, 'High': High, 'Low': Low, 'total': self.crypto_bought, 'type': "buy"})
 
         elif action == 2 and self.crypto_held > 0:
             # Sell 100% of current crypto held
             self.crypto_sold = self.crypto_held
             self.balance += self.crypto_sold * current_price
             self.crypto_held -= self.crypto_sold
-            self.trades.append({'Date': Date, 'High': High, 'Low': Low, 'total': self.crypto_sold, 'type': "sell"})
 
         self.prev_net_worth = self.net_worth
         self.net_worth = self.balance + self.crypto_held * current_price
 
         self.orders_history.append(
             [self.balance, self.net_worth, self.crypto_bought, self.crypto_sold, self.crypto_held])
-        # Write_to_file(Date, self.orders_history[-1])
 
         # Calculate reward
         reward = self.net_worth - self.prev_net_worth
@@ -123,26 +112,17 @@ class CustomEnv:
         return obs, reward, done
 
     # render environment
-    def render(self, visualize=False):
-        # print(f'Step: {self.current_step}, Net Worth: {self.net_worth}')
-        if visualize:
-            Date = self.df.loc[self.current_step, 'Date']
-            Open = self.df.loc[self.current_step, 'Open']
-            Close = self.df.loc[self.current_step, 'Close']
-            High = self.df.loc[self.current_step, 'High']
-            Low = self.df.loc[self.current_step, 'Low']
-            Volume = self.df.loc[self.current_step, 'Volume']
-
-            # Render the environment to the screen
-            self.visualization.render(Date, Open, High, Low, Close, Volume, self.net_worth, self.trades)
+    def render(self):
+        print(f'Step: {self.current_step}, Net Worth: {self.net_worth}')
 
 
-def Random_games(env, visualize, train_episodes=50, training_batch_size=500):
+def Random_games(env, train_episodes=50, training_batch_size=500):
     average_net_worth = 0
     for episode in range(train_episodes):
         state = env.reset(env_steps_size=training_batch_size)
+
         while True:
-            env.render(visualize)
+            env.render()
 
             action = np.random.randint(3, size=1)[0]
 
@@ -159,11 +139,11 @@ def Random_games(env, visualize, train_episodes=50, training_batch_size=500):
 df = pd.read_csv('historical_prices/pricedata.csv')
 df = df.sort_values('Date')
 
-lookback_window_size = 50
+lookback_window_size = 10
 train_df = df[:-720 - lookback_window_size]
 test_df = df[-720 - lookback_window_size:]  # 30 days
 
 train_env = CustomEnv(train_df, lookback_window_size=lookback_window_size)
 test_env = CustomEnv(test_df, lookback_window_size=lookback_window_size)
 
-Random_games(test_env, visualize=True, train_episodes=1, training_batch_size=300)
+Random_games(train_env, train_episodes=10, training_batch_size=500)
